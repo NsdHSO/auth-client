748a7a336a3adef9f3519a208c8d0736b3c519d0
nsdHso <nechiforelsamuel@gmail.com>
2025-12-26 16:19:27 +0200

    feat: 🎸 added service register   #1

--- apps/auth/src/app/layout/register/register.ts
+++ apps/auth/src/app/layout/register/register.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Register as RegisterLogic } from './service/register';
+
 @Component({
   selector: 'app-register',
   imports: [CommonModule, RouterLink],
