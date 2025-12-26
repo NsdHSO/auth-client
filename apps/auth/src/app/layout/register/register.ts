591d9a44847e2010ea65c4be09ebc1544025ca84
nsdHso <nechiforelsamuel@gmail.com>
2025-12-26 14:59:59 +0200

    f added provider for register  #1

--- apps/auth/src/app/layout/register/register.ts
+++ apps/auth/src/app/layout/register/register.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
-
+import { Register as RegisterLogic } from './service/register';
 @Component({
   selector: 'app-register',
   imports: [CommonModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
+  providers: [RegisterLogic],
 })
 export class Register {}
