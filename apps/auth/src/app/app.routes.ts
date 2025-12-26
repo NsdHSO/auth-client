10ffe45e7ad22dc429234522f2a0008416da936a
nsdHso <nechiforelsamuel@gmail.com>
2025-12-26 14:52:39 +0200

    f added template for register and redirect to login if have an account closes #1

--- apps/auth/src/app/app.routes.ts
+++ apps/auth/src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
-	{
-		path: 'login',
-		loadComponent: () => import('./layout/login/login').then((c) => c.Login),
-	},
+  {
+    path: 'login',
+    loadComponent: () => import('./layout/login/login').then((c) => c.Login),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./layout/register/register').then((c) => c.Register),
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-
 ];
--- apps/auth/src/app/layout/register/register.ts
+++ apps/auth/src/app/layout/register/register.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
